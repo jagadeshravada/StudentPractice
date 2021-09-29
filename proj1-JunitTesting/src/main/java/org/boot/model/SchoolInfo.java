2e28eb92618eba3e856f08ded50ce3f91726e885
ASUS <ASUS@ASUS>
2021-09-29 10:19:20 +0530

    schoolinfo added to B2-fun2

---
+++ proj1-JunitTesting/src/main/java/org/boot/model/SchoolInfo.java
@@ -0,0 +1,18 @@
+package org.boot.model;
+
+public class SchoolInfo {
+	
+	
+	private Integer id;
+	private String schoolname;
+	private String licenno;
+	public SchoolInfo(Integer id, String schoolname, String licenno) {
+		//super();
+		this.id = id;
+		this.schoolname = schoolname;
+		this.licenno = licenno;
+	}
+	
+	
+
+}
